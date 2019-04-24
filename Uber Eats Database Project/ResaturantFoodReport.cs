bbd08c06988d5cc374a58f3aaaf9bacfbfc7665e
Heshamovic <mohamed.97198@gmail.com>
2019-04-24 21:56:52 +0200

    adding Restauarnt food report and finishing delivered orders report

--- Uber Eats Database Project/ResaturantFoodReport.cs
+++ Uber Eats Database Project/ResaturantFoodReport.cs
@@ -16,14 +16,14 @@ namespace Uber_Eats_Database_Project {
     using CrystalDecisions.CrystalReports.Engine;
     
     
-    public class RestaurantFood : ReportClass {
+    public class ResaturantFoodReport : ReportClass {
         
-        public RestaurantFood() {
+        public ResaturantFoodReport() {
         }
         
         public override string ResourceName {
             get {
-                return "RestaurantFood.rpt";
+                return "ResaturantFoodReport.rpt";
             }
             set {
                 // Do nothing
@@ -41,7 +41,7 @@ namespace Uber_Eats_Database_Project {
         
         public override string FullResourceName {
             get {
-                return "Uber_Eats_Database_Project.RestaurantFood.rpt";
+                return "Uber_Eats_Database_Project.ResaturantFoodReport.rpt";
             }
             set {
                 // Do nothing
@@ -66,7 +66,7 @@ namespace Uber_Eats_Database_Project {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
+        public CrystalDecisions.CrystalReports.Engine.Section GroupHeaderSection1 {
             get {
                 return this.ReportDefinition.Sections[2];
             }
@@ -74,7 +74,7 @@ namespace Uber_Eats_Database_Project {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
+        public CrystalDecisions.CrystalReports.Engine.Section GroupHeaderSection2 {
             get {
                 return this.ReportDefinition.Sections[3];
             }
@@ -82,17 +82,73 @@ namespace Uber_Eats_Database_Project {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
             get {
                 return this.ReportDefinition.Sections[4];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section GroupFooterSection2 {
+            get {
+                return this.ReportDefinition.Sections[5];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section GroupFooterSection1 {
+            get {
+                return this.ReportDefinition.Sections[6];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
+            get {
+                return this.ReportDefinition.Sections[7];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section Section5 {
+            get {
+                return this.ReportDefinition.Sections[8];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_RestaurantName {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_FromDate {
+            get {
+                return this.DataDefinition.ParameterFields[1];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_ToDate {
+            get {
+                return this.DataDefinition.ParameterFields[2];
             }
         }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
-    public class CachedRestaurantFood : Component, ICachedReport {
+    public class CachedResaturantFoodReport : Component, ICachedReport {
         
-        public CachedRestaurantFood() {
+        public CachedResaturantFoodReport() {
         }
         
         [Browsable(false)]
@@ -129,7 +185,7 @@ namespace Uber_Eats_Database_Project {
         }
         
         public virtual CrystalDecisions.CrystalReports.Engine.ReportDocument CreateReport() {
-            RestaurantFood rpt = new RestaurantFood();
+            ResaturantFoodReport rpt = new ResaturantFoodReport();
             rpt.Site = this.Site;
             return rpt;
         }
