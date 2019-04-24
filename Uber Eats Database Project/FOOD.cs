49ad090a14920cc61288b04e4f3a9ad6a2433e56
Heshamovic <mohamed.97198@gmail.com>
2019-04-24 19:30:14 +0200

    Revert "order"
    This reverts commit 6c873a5c644a2b9d90a735e8d3b15c20580e8494.

--- Uber Eats Database Project/FOOD.cs
+++ Uber Eats Database Project/FOOD.cs
@@ -7,29 +7,26 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Uber_Eats_Database_Project
+using System;
+using System.Collections.Generic;
+
+public partial class FOOD
 {
-    using System;
-    using System.Collections.Generic;
-    
-    public partial class FOOD
+    public FOOD()
     {
-        public FOOD()
-        {
-            this.ORDER_FOOD = new HashSet<ORDER_FOOD>();
-        }
-    
-        public string FOOD_NAME { get; set; }
-        public string RESTAURANT_NAME { get; set; }
-        public string RESTAURANT_LOCATION { get; set; }
-        public Nullable<decimal> PRICE { get; set; }
-        public Nullable<decimal> DISCOUNT { get; set; }
-        public Nullable<decimal> RATING { get; set; }
-        public string INGREDIANTS { get; set; }
-        public string TOP_DISH { get; set; }
-        public string FOODIMAGE { get; set; }
-    
-        public virtual RESTAURANT RESTAURANT { get; set; }
-        public virtual ICollection<ORDER_FOOD> ORDER_FOOD { get; set; }
+        this.ORDER_FOOD = new HashSet<ORDER_FOOD>();
     }
+
+    public string FOOD_NAME { get; set; }
+    public string RESTAURANT_NAME { get; set; }
+    public string RESTAURANT_LOCATION { get; set; }
+    public Nullable<decimal> PRICE { get; set; }
+    public Nullable<decimal> DISCOUNT { get; set; }
+    public Nullable<decimal> RATING { get; set; }
+    public string INGREDIANTS { get; set; }
+    public string TOP_DISH { get; set; }
+    public string FOODIMAGE { get; set; }
+
+    public virtual RESTAURANT RESTAURANT { get; set; }
+    public virtual ICollection<ORDER_FOOD> ORDER_FOOD { get; set; }
 }
