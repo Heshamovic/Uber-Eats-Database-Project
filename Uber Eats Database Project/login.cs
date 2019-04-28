b0a77b6ce1e62498bc4859b901d761a6337b0f1b
Heshamovic <mohamed.97198@gmail.com>
2019-04-28 18:48:35 +0200

    fixing framework installation

--- Uber Eats Database Project/login.cs
+++ Uber Eats Database Project/login.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Oracle.ManagedDataAccess.Client;
+using Oracle.DataAccess.Client;
 using Oracle.DataAccess.Types;
 using Oracle.ManagedDataAccess.EntityFramework;
 
